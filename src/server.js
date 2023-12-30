6c85fc59ebb4fab07645e8283447096221248005
Shivansh10000 <shivanshj157@gmail.com>
2023-12-30 19:11:58 +0530

    keyword search

--- src/server.js
+++ src/server.js
@@ -9,6 +9,9 @@ import session from 'express-session';
 import MongoDBStorePackage from 'connect-mongodb-session';
 import mongoSanitize from 'express-mongo-sanitize';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
+import OpenAI from "openai";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 if(process.env.NODE_ENV !== 'production') {
@@ -29,6 +32,10 @@ app.use(helmet.originAgentCluster());
 app.use(helmet.permittedCrossDomainPolicies());
 app.use(helmet.referrerPolicy());
 app.use(helmet.xssFilter());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json())
+app.use(cors())
 
 const scriptSrcUrls = [
   "https://stackpath.bootstrapcdn.com/",
@@ -126,6 +133,49 @@ app.use(session({
 app.get("/",  (req, res) => {
   res.send("Hello, Welcome to My backend!!");
 })
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.post("/extract-keywords", async (req, res) => {
+    try {
+        const {prompt} = req.body
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                "role": "system",
+                "content": "You will be provided with a block of text, and your task is to extract a list of keywords from it."
+              },
+              {
+                "role": "user",
+                "content": `${prompt}`
+              }
+            ],
+            temperature: 0.5,
+            max_tokens: 64,
+            top_p: 1,
+          });
+
+
+        return res.status(200).json({
+            success: "true",
+            data: response.choices[0].message.content
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: "false",
+            error: error.response ? error.response.data : "There was an issue with the server"
+        })
+    }
+})
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 
 app.listen(3000, () => {
